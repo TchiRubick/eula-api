2f08b4c259c405c856f788575ee987e6cdde3b8c
Tchi <tchi.devica@gmail.com>
2021-11-16 16:00:14 +0100

    inventories

--- src/routes/inventory/inventory.private.route.ts
+++ src/routes/inventory/inventory.private.route.ts
@@ -3,8 +3,9 @@ import { validate, Joi } from 'express-validation';
 
 import privateCheckMiddleware from '~/middlewares/privateCheckMiddleware';
 import * as inventoryRepository from '~/services/inventories/inventory.repository';
-import { transformToAdmin } from '~/services/inventories/inventory.transformer';
+import { transformToAdmin, transformManyToAdmin } from '~/services/inventories/inventory.transformer';
 import * as iI from '~/services/inventories/inventory.interface';
+import { getPaginationStats } from '~/services/pagination/pagination.service';
 
 const router = Router();
 
@@ -78,6 +79,58 @@ router.put('/:barcode',
     }
 
     return res.json({ inventory: transformToAdmin(inventoryUpdated) });
+  });
+
+const filterValidation = {
+  query: Joi.object({
+    search: Joi.string(),
+    page: Joi.number(),
+    size: Joi.number().max(60),
+  }),
+};
+
+type requestFilter = {
+  search?: string
+  page?: string
+  size?: string
+};
+
+router.get('/', privateCheckMiddleware, validate(filterValidation, {}, {}), async (req: Request, res: Response) => {
+  const { search, page: qPage, size: qSize }: requestFilter = req.query;
+
+  const page = qPage ? parseInt(qPage, 10) : 0;
+  const size = qSize ? parseInt(qSize, 10) : 0;
+
+  const inventories = await inventoryRepository.getByFilter(search, page, size);
+
+  if (inventories instanceof Error) {
+    return res.status(422).json({ error: inventories.message, message: 'Cannot get list of inventories' });
+  }
+
+  const count = await inventoryRepository.getCount();
+
+  return res.json({ users: transformManyToAdmin(inventories), stats: getPaginationStats(page, size, count) });
+});
+
+const detailsValidation = {
+  params: Joi.object({
+    barcode: Joi.string().alphanum().min(10).max(15),
+  }),
+};
+
+router.get('/:barcode',
+  privateCheckMiddleware,
+  validate(detailsValidation, {}, {}),
+  async (req: Request, res: Response) => {
+    const { barcode } = req.params;
+
+    const inventory = await inventoryRepository.getOne({ barcode });
+
+    if (inventory instanceof Error) {
+      return res.status(422).json({ error: inventory.message, message: 'Cannot get inventory' });
+    }
+
+    return res.json({ users: transformToAdmin(inventory) });
   });
 
 export default router;
--- src/services/inventories/inventory.repository.ts
+++ src/services/inventories/inventory.repository.ts
@@ -2,6 +2,7 @@ import logger from '~/utils/logger/logger.util';
 
 import Inventory from './inventory.model';
 import * as iI from './inventory.interface';
+import { likeFormatter, escapeRegexString } from '~/helpers/index.helper';
 
 export const create = async (props: iI.iReqInv): Promise<iI.iInv | Error> => {
   let result: iI.iInv | Error;
@@ -62,3 +63,47 @@ export const updateOne = async (where: any | unknown, params: iI.iReqUpdateInv):
 };
 
 export default create;
+
+export const getByFilter = async (search?: string, page?: number, size = 30): Promise<iI.iInv[] | Error> => {
+  let result: iI.iInv[] | Error;
+
+  try {
+    let skip = 0;
+
+    if (page) {
+      skip = (page - 1) * size;
+    }
+
+    let where: any;
+
+    if (search) {
+      const escapedSearch = escapeRegexString(search);
+      const likeSearch = likeFormatter(escapedSearch);
+
+      where = {
+        $or: [
+          { barcode: { $regex: likeSearch, $options: 'i' } },
+          { name: { $regex: likeSearch, $options: 'i' } },
+        ],
+      };
+    }
+
+    result = await Inventory.find(where, null, { skip, limit: size });
+
+    if (!result) {
+      throw Error('Inventories not found');
+    }
+  } catch (error: any) {
+    logger.warning(error.message);
+
+    result = Error(error.message);
+  }
+
+  return result;
+};
+
+export const getCount = async (where?: any | unknown): Promise<number> => {
+  const count: number = await Inventory.countDocuments(where ?? null);
+
+  return count ?? 0;
+};
